596c119149d38b4fbbb87ed52845748a1b025756
wonjs <wonjs@sk.com>
2022-11-09 11:18:33 +0900

    Updates

---
+++ precache-manifest.c3b519606d125aabc7d61e23ed91fedc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "009c777dc2ee3a319506",
+    "url": "/walletconnect-dapp/static/js/main.009c777d.chunk.js"
+  },
+  {
+    "revision": "736b76290454fbdf84d6",
+    "url": "/walletconnect-dapp/static/js/1.736b7629.chunk.js"
+  },
+  {
+    "revision": "3dc5a7f0592f67714a51",
+    "url": "/walletconnect-dapp/static/js/runtime~main.3dc5a7f0.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/walletconnect-dapp/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/walletconnect-dapp/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "42bca7317690c3f5e9ecd8db387344df",
+    "url": "/walletconnect-dapp/static/media/eth.42bca731.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/walletconnect-dapp/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "0b93b13c7f83bac46db63ff09bab5ad3",
+    "url": "/walletconnect-dapp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/walletconnect-dapp/workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/walletconnect-dapp/workbox-v3.6.3"});
+
+importScripts(
+  "/walletconnect-dapp/precache-manifest.c3b519606d125aabc7d61e23ed91fedc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/walletconnect-dapp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
